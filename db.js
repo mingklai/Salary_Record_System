c8236b37658b8bb28307153bd0e21561649cff84
mingklai <102860860+mingklai@users.noreply.github.com>
2024-07-05 22:23:33 +0800

    version 1.5
    add report function (search other month)

--- db.js
+++ db.js
@@ -1,4 +1,11 @@
-//Unique Firebase Object
+function index_init(){
+  let nowTime = new Date();
+  Col_Name = nowTime.getFullYear() + "-" + ("0" + (nowTime.getMonth() + 1)).slice(-2);
+
+  document.getElementById("showThisMonth_btn").innerHTML += " (" + Col_Name + ")";
+}
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyArX70jSDxA7eBzdsJsQglPSskQvUNOM98",
     authDomain: "salary-record-system.firebaseapp.com",
@@ -14,17 +21,21 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   var firestore = firebase.firestore();
   
-  //Variable to access database collection
-  const db = firestore.collection("testData");
-  
+
   //Get Submit Form
-  let submitButton = document.getElementById("submit");
+  let submitButton = document.getElementById("submit"); 
   
+  var db_Collection;
+
+ 
   //Create Event Listener To Allow Form Submission
   submitButton.addEventListener("click", (e) => {
     //Prevent Default Form Submission Behavior
     if(true){
       e.preventDefault();
+
+      db_Collection = document.getElementById("date").value.substring(0,7);
+      var db = firestore.collection(db_Collection);
   
       //Get Form Values
       let date = String(document.getElementById("date").value);
@@ -59,19 +70,22 @@ const firebaseConfig = {
     
   });
 
+
   function clearForm() {
     document.getElementById("clearForm").reset();
-}
+  }
 
 
 function showAllRecord(){
+
+    db_Collection = Col_Name;
     document.getElementById("all_data").innerHTML = "<tr><th>日期</th><th>返工</th><th>收工</th><th>時數</th><th>時薪</th><th>當天人工</th></tr></tr>";
 
     var d,sT,oT,Salary;
     var totalMin = 0;
     var totalSalary = 0;
     firestore
-        .collection("testData")
+        .collection(db_Collection)
         .get()
         .then((snapshot) => {
         snapshot.docs.forEach((doc) => {
@@ -117,29 +131,78 @@ function showAllRecord(){
 
 }
 
-// function checkForm(){
-//   console.log("runnning checkForm");
-//   console.log(String(document.getElementById("date")));
-//   console.log(String(document.getElementById("clockStartTime")));
-//   console.log(String(document.getElementById("clockEndTime")));
-//   console.log(String(document.getElementById("SalaryPerHour")));
+function showReport(){
+  document.getElementById("all_data_report").innerHTML = "<tr><th>日期</th><th>返工</th><th>收工</th><th>時數</th><th>時薪</th><th>當天人工</th></tr></tr>";
+  var check_month = document.getElementById("calendar").value;
+  console.log(check_month);
+  var d,sT,oT,Salary;
+  var totalMin = 0;
+  var totalSalary = 0;
+  firestore
+      .collection(check_month)
+      .get()
+      .then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+          d = doc.data().date;
+          sT = doc.data().startTime;
+          oT = doc.data().offTime;
+          Salary = doc.data().SalaryPerHour;
 
-//   if(String(document.getElementById("date")) == null){
-//       alert("未入日期");
-//       return false;
-//   }
-//   if(String(document.getElementById("clockStartTime")) == null){
-//       alert("未入返工時間");
-//       return false;
-//   }
-//   if(String(document.getElementById("clockEndTime")) == null){
-//       alert("未入返工時間");
-//       return false;
-//   }
-//   if(String(document.getElementById("SalaryPerHour")) == null){
-//       alert("未入時薪");
-//       return false;
-//   }
-//   //if(sT !> oT)
-//   return true
-// }
+          var sHour = Number(sT.substring(0,2));
+          var sMin = Number(sT.substring(3,6));
+          var oHour = Number(oT.substring(0,2));
+          var oMin = Number(oT.substring(3,6));
+          var dayHour = Math.floor(((oHour*60 + oMin) - (sHour*60 + sMin))/60);
+          var dayMin = ((oHour*60 + oMin) - (sHour*60 + sMin))%60;
+          var daySalary = (dayHour + (dayMin/60)) * Salary;
+          totalMin = totalMin + dayHour*60 + dayMin;
+          totalSalary = totalSalary + daySalary;
+          document.getElementById("all_data_report").innerHTML = document.getElementById("all_data_report").innerHTML
+          + "<tr>" 
+          + "<td>" + d + "</td>" 
+          + "<td>" + sT + "</td>" 
+          + "<td>" + oT + "</td>"
+          + "<td>" + dayHour + "h " + dayMin  + "m" + "</td>"
+          + "<td>" + Salary + "</td>"
+          + "<td>" + daySalary + "</td>"
+          + "</tr>";
+      });
+      var totalshowHour = Math.floor((totalMin/60));
+      var totalshowMin = totalMin%60;
+      document.getElementById("all_data_report").innerHTML = document.getElementById("all_data_report").innerHTML 
+      + "<tr>" 
+          + "<td>" + "-" + "</td>" 
+          + "<td>" + "-" + "</td>" 
+          + "<td>" + "Total" + "</td>"
+          + "<td style=\"color:red;\">" + totalshowHour + "h " + totalshowMin  + "m" +  "</td>"
+          + "<td>" + "Total" + "</td>"
+          + "<td style=\"color:red;\">" + totalSalary + "</td>"
+          + "</tr>";
+
+  });
+}
+
+function checkForm(){
+  console.log("runnning checkForm");
+  let date = String(document.getElementById("date").value);
+  let startTime = String(document.getElementById("clockStartTime").value);
+  let offTime = String(document.getElementById("clockOffTime").value);
+  if(document.getElementById("date") == null){
+      alert("未入日期");
+      return;
+  }
+  if(document.getElementById("clockStartTime")== null){
+      alert("未入返工時間");
+      return;
+  }
+  if(document.getElementById("clockEndTime") == null){
+      alert("未入返工時間");
+      return;
+  }
+  if(document.getElementById("SalaryPerHour") == null){
+      alert("未入時薪");
+      return;
+  }
+  //if(sT !> oT)
+  submit();
+};
